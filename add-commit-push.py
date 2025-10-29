91955a722ff5b3c627723271d906cd52b3063568
Ethan Fisher <ethanfisher@Ethans-MacBook-Pro.local>
2025-10-29 10:37:38 -0500

    "update files."

--- add-commit-push.py
+++ add-commit-push.py
@@ -5,3 +5,9 @@ subprocess.run(["git", "status"])
 
 print ("git add -A")
 subprocess.run(["git", "add", "-A"])
+
+print ("git commit -m")
+subprocess.run(["git", "commit", "-m", "\"update files.\""])
+
+print ("git push")
+subprocess.run(["git", "push"])
