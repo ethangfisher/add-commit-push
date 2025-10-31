24db15a7ba043d7e9cd7bed93fee30fafb46a45f
Ethan Fisher <ethanfisher@Ethans-MacBook-Pro.local>
2025-10-31 10:52:29 -0500

    "update files."

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,7 +1,19 @@
 import subprocess
+import sys
+
 print ("Starting add-commit-push")
 print ("git status")
 subprocess.run(["git", "status"])
+
+# Ask the user if they want to continue
+confirm = input("Do you want to continue? (y/n): ")
+
+if confirm.lower() == "y":
+    print("Continuing with add commit push")
+else:
+    print("Exiting...")
+    sys.exit()
+    
 
 print ("git add -A")
 subprocess.run(["git", "add", "-A"])
