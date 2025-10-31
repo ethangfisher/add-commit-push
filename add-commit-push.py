670c4ef59da1f4c6224e3b153705cfa82ed6fdaf
Ethan Fisher <ethanfisher@Ethans-MacBook-Pro.local>
2025-10-31 11:04:46 -0500

    Update files.

--- add-commit-push.py
+++ add-commit-push.py
@@ -2,6 +2,13 @@ import subprocess
 import sys
 
 Message = "Update files."
+# Check if the user has a parameter of m for message
+if len(sys.argv) > 1 and sys.argv[1] == "-m":
+    if len(sys.argv) > 2:
+        Message = sys.argv[2]
+    else:
+        print("Error: No commit message provided after -m")
+        sys.exit(1)
 
 print ("Starting add-commit-push")
 print ("git status")
@@ -25,4 +32,3 @@ subprocess.run(["git", "commit", "-m", Message])
 
 print ("git push")
 subprocess.run(["git", "push"])
-
